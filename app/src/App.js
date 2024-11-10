1a3d0dcf60da17361739abb9106c4394e7aef503
ryanzhang-200 <ryanzhang34567@gmail.com>
2024-11-10 13:51:13 -0800

    TextBoxes

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+import React, { useState, setText } from 'react';
 import './App.css';
+
+const TextBox = () => {
+  const [text, setText] = useState("");
+  const handleChange = (event) => {
+    setText(event.target.value); 
+  };
+
+  return (
+    <div className = "TextBox">
+      <label htmlFor="TextBox">Enter text: </label>
+      <input
+        type="text"
+        id="textBox"
+        value={text}          
+        onChange={handleChange}
+        placeholder="Type something here..."
+      />
+      <p>You typed: {text}</p> {}
+    </div>
+  );
+}
+
+const Header = ({ time }) => {
+  return (
+    <div className="App-header">
+      <h1>Symptoms</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <TextBox />
+      <TextBox />
+      <TextBox />
     </div>
   );
 }
