4852f6f42c2b88a5f71f26bd63828ccc9736cc71
Mu Ye Liu <muyeliu1208@gmail.com>
2024-11-10 11:26:56 -0800

    commit 2

---
+++ model/symptoms.h
@@ -0,0 +1,29 @@
+#ifndef SYMPTOMS_H
+#define SYMPTOMS_H
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <set>
+#include <string>
+#include <new>
+#include <utility>
+#include <map>
+
+using namespace std;
+
+/*
+This will be class the contains the dictionary that maps the inaccurrate 
+description of symptoms to medical terms for the symptoms
+*/
+
+class Symptoms {
+public:
+
+private:
+
+    map<string, set<string>> terms;
+
+};
+
+#endif
