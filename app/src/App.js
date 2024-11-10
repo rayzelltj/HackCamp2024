2b48bae99ad52440b13785d6b831e18ca7c718fa
Rayzell Tjandra <90327294+rayzelltj@users.noreply.github.com>
2024-11-10 15:59:01 -0800

    modified component changes to loading stage

--- app/src/App.js
+++ app/src/App.js
@@ -1,41 +1,48 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const TextBox = () => {
+const TextBox = ({ loading, setLoading }) => {
   const [text, setText] = useState("");
+  const [response, setResponse] = useState(false);
 
-  // Handles the change event for the input field
   const handleChange = (event) => {
     setText(event.target.value); 
   };
 
-  // Handles the keydown event for the input field
   const handleKeyDown = (event) => {
     if (event.key === 'Enter' && text.trim() !== "") {
-      // Change the URL or navigate to a new page
-      // Example: Change to your desired URL
-      window.location.href = `/newpage?query=${encodeURIComponent(text)}`;
+      setLoading(true);
+      // Simulate loading or navigate to another page here
+      // e.g., window.location.href = `/newpage?query=${encodeURIComponent(text)}`;
     }
   };
 
-  return (
-    <div className="TextBox">
-      <input
-        type="text"
-        id="textBox"
-        value={text}          
-        onChange={handleChange}
-        onKeyDown={handleKeyDown} // Added keydown event listener
-        className="input-field"
-        placeholder='e.g. "I feel dizzy and my chest feels tight"'
-        autoFocus
-      />
+  if (loading) return null; // Do not render if loading is true
 
-    </div>
+  return (
+    <>
+      {response ? (
+        <p>Response</p>
+      ) : (
+        <div className="TextBox">
+          <input
+            type="text"
+            id="textBox"
+            value={text}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            className="input-field"
+            placeholder='e.g. "I feel dizzy and my chest feels tight"'
+            autoFocus
+          />
+        </div>
+      )}
+    </>
   );
 };
 
-const Header = () => {
+const Header = ({ loading }) => {
+  if (loading) return null; // Do not render if loading is true
   return (
     <div className="App-header">
       <h1>MedLex</h1>
@@ -45,10 +52,18 @@ const Header = () => {
 };
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
-      <Header />
-      <TextBox />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Header loading={loading} />
+          <TextBox loading={loading} setLoading={setLoading} />
+        </>
+      )}
     </div>
   );
 }
